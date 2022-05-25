0773a7abaecac6c34aec6fe737abb92c8f34ff3f
lilibella <lilibella28@hotmail.com>
2022-05-25 14:15:55 -0700

    new files

---
+++ src/pages/FeedPage/FeedPage.jsx
@@ -0,0 +1,126 @@
+import React, { useState, useEffect } from "react";
+
+import PageHeader from "../../components/Header/Header";
+import AddPostForm from "../../components/AddPostForm/AddPostForm";
+import PostGallery from "../../components/PostGallery/PostGallery";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import Loading from "../../components/Loader/Loader";
+import * as postsAPI from "../../utils/postApi";
+import * as likesAPI from '../../utils/likeApi';
+
+
+
+
+import { Grid } from "semantic-ui-react";
+
+
+
+export default function Feed({user, handleLogout}) {
+  const [posts, setPosts] = useState([]); 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+
+  async function addLike(postId){
+    try {
+      const data = await likesAPI.create(postId)
+      getPosts(); 
+    } catch(err){
+      console.log(err)
+      setError(err.message)
+    }
+  }
+
+  async function removeLike(likeId){
+    try {
+      const data = await likesAPI.removeLike(likeId);
+      getPosts()
+      
+    } catch(err){
+      console.log(err);
+      setError(err.message);
+    }
+  }
+
+
+
+  //  in Crud
+  async function handleAddPost(post) {
+    try {
+      setLoading(true);
+      const data = await postsAPI.create(post); 
+      
+      setPosts([data.post, ...posts]);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setError(err.message);
+    }
+  }
+
+  // R read in crud
+  async function getPosts() {
+    try {
+      const data = await postsAPI.getAll();
+      console.log(data, " this is data,");
+      setPosts([...data.posts]);
+      setLoading(false);
+    } catch (err) {
+      console.log(err.message, " this is the error");
+      setError(err.message);
+    }
+  }
+
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+
+
+  if (error) {
+    return (
+      <>
+        <PageHeader handleLogout={handleLogout} user={user}/>
+        <ErrorMessage error={error} />;
+      </>
+    );
+  }
+
+  if (loading) {
+    return (
+      <>
+        <PageHeader handleLogout={handleLogout} user={user}/>
+        <Loading />
+      </>
+    );
+  } 
+
+  return (
+    <Grid centered>
+      <Grid.Row>
+        <Grid.Column>
+          <PageHeader handleLogout={handleLogout} user={user}/>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <AddPostForm handleAddPost={handleAddPost} />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <PostGallery
+            posts={posts}
+            numPhotosCol={1}
+            isProfile={false}
+            loading={loading}
+            addLike={addLike}
+            removeLike={removeLike}
+            user={user}
+          />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+}
---
+++ src/pages/SignupPage/SignupPage.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
+import { Button, Form, Grid, Header, Image, Segment } from 'semantic-ui-react';
+import userService from '../../utils/userService';
+import { useNavigate } from 'react-router-dom';
+
+export default function SignUpPage(props) {
+	const navigate = useNavigate();
+
+	const [error, setError] = useState('');
+	const [state, setState] = useState({
+		username: '',
+		email: '',
+		password: '',
+		passwordConf: '',
+		bio: '',
+	});
+
+	const [selectedFile, setSelectedFile] = useState('');
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		const formData = new FormData(); 
+		formData.append('photo', selectedFile);
+		for (let fieldName in state) {
+			formData.append(fieldName, state[fieldName]);
+		}
+
+		try {
+			await userService.signup(formData);
+
+			props.handleSignUpOrLogin();
+
+			navigate('/');
+		} catch (err) {
+			setError(err.message);
+		}
+	}
+
+	function handleChange(e) {
+		setState({
+			...state,
+			[e.target.name]: e.target.value,
+		});
+	}
+
+	function handleFileInput(e) {
+		console.log(e.target.files);
+		setSelectedFile(e.target.files[0]);
+	}
+
+	return (
+		<Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="middle">
+			<Grid.Column style={{ maxWidth: 450 }}>
+				<Header as="h2" color="orange" textAlign="center">
+					<Image src="https://i.imgur.com/yPoEdxZ.png"/> Sign Up
+				</Header>
+				<Form autoComplete="off" onSubmit={handleSubmit}>
+					<Segment stacked>
+						<Form.Input
+							name="username"
+							placeholder="username"
+							value={state.username}
+							onChange={handleChange}
+							required
+						/>
+						<Form.Input
+							type="email"
+							name="email"
+							placeholder="email"
+							value={state.email}
+							onChange={handleChange}
+							required
+						/>
+						<Form.Input
+							name="password"
+							type="password"
+							placeholder="password"
+							value={state.password}
+							onChange={handleChange}
+							required
+						/>
+						<Form.Input
+							name="passwordConf"
+							type="password"
+							placeholder="Confirm Password"
+							value={state.passwordConf}
+							onChange={handleChange}
+							required
+						/>
+						<Form.TextArea
+							label="bio"
+							name="bio"
+							placeholder="Tell us more about you..."
+							onChange={handleChange}
+						/>
+						<Form.Field>
+							<Form.Input
+								type="file"
+								name="photo"
+								placeholder="upload image"
+								onChange={handleFileInput}
+							/>
+						</Form.Field>
+						<Button type="submit" className="btn">
+							Signup
+						</Button>
+					</Segment>
+					{error ? <ErrorMessage error={error} /> : null}
+				</Form>
+			</Grid.Column>
+		</Grid>
+	);
+}
